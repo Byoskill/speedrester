dcbecd2ba99d3cc7387d94f2b47ffe216d64366d
Sylvain Leroy <sleroy0@gmail.com>
2020-12-08 00:23:22 +0100

    backup

--- speed-rester-tool/src/swagger-loader.js
+++ speed-rester-tool/src/swagger-loader.js
@@ -1,25 +1,42 @@
 /**
  * Byoskill Leroy - All rights reserved 2020-2021
  */
+const util = require('util');
+const axios = require('axios').default;
 const fs = require('fs');
 const yaml = require('js-yaml');
 
- /**
+fs.readFileAsync = util.promisify(fs.readFile);
+
+/**
   * This component is in charge to load a swagger/OpenAPI definition from an REST API.
   */
-export default class SwaggerLoader {
+class SwaggerLoader {
 
-    constructor() {
+    /**
+     * Provides the type yml|json
+     */
+    constructor(expectedType) {
+        this.expectedType = expectedType;
 
     }
 
+    /**
+     * Loads a swagger from an URL and returns a promise with the swagger info.
+     * @param {url} the url to the swagger
+     */
     loadFromUrl(url) {
+        
+        return axios.get(url)
+            .then(response => this.handleUrlResponse(response));
+
 
     }
 
     loadFromFile(file) {
 
-        
+        return fs.readFileAsync(file)
+            .then(response => this.handleFileResource(response));
     }
 
     /**
@@ -29,5 +46,23 @@ export default class SwaggerLoader {
         return true;
     }
 
+    handleUrlResponse(response) {
+        if (this.expectedType == 'json') {
+            return response.data;
+        }
+        return response.data;
+
+    }
+
+
+    handleFileResource(response) {
+        if (this.expectedType == 'json') {
+            return JSON.parse(response + '');
+        }
+        return response.data;
+
+    }
 
 }
+
+module.exports.SwaggerLoader = SwaggerLoader;
---
+++ speed-rester-tool/src/testgenerator.js
@@ -0,0 +1,9 @@
+/**
+ * This class defines the code to generate the REST tests.
+ */
+
+
+
+
+
+exports.default = TestGenerator;
